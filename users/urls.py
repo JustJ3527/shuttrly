733ec5826a23213b2759b39aa3914cb3e105ca42
JustJ3527 <antoine.jules04@gmail.com>
2025-07-31 02:02:41 +0200

    Bug urls.py admin solved

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),  # Custom login view
     path('profile/', profile_view, name='profile'),  # Profile view for logged-in users
